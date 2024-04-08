2118a923c43085d9a1a795c159a849921eb43db9
Bolshov92 <bolshov92@gmail.com>
2024-04-08 21:05:50 +0100

    add controller Employee,add package Repository and Service

---
+++ src/main/java/com/example/construction_company_management/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.construction_company_management.service;
+
+import com.example.construction_company_management.entity.Employee;
+
+import java.util.UUID;
+
+public interface EmployeeService {
+    Employee getEmployeeById(UUID a);
+}
---
+++ src/main/java/com/example/construction_company_management/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.construction_company_management.service.impl;
+
+import com.example.construction_company_management.entity.Employee;
+import com.example.construction_company_management.repository.EmployeeRepository;
+import com.example.construction_company_management.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    @Override
+    public Employee getEmployeeById(UUID id) {
+        return employeeRepository.getEmployeeById(id);
+    }
+}
