27d1adc2300fa3fc73ee166acd75614c6a5d2257
Bolshov92 <bolshov92@gmail.com>
2024-04-08 18:19:27 +0100

    Added test data to database tables
    Test data has been added to all database tables to verify the functionality and correctness of the application.

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,31 +1,31 @@
 CREATE TABLE authority
 (
     id        CHAR(36) PRIMARY KEY,
-    authority VARCHAR(255) NOT NULL UNIQUE
+    authority VARCHAR(255)
 );
 
 
 CREATE TABLE roles
 (
     role_id   CHAR(36) PRIMARY KEY,
-    role_name VARCHAR(255) NOT NULL UNIQUE
+    role_name VARCHAR(255)
 );
 
 
 CREATE TABLE user_info
 (
     user_id      CHAR(36) PRIMARY KEY,
-    user_name    VARCHAR(255) NOT NULL UNIQUE,
-    password     VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20)  NOT NULL UNIQUE
+    user_name    VARCHAR(255),
+    password     VARCHAR(255),
+    phone_number VARCHAR(20) NOT NULL UNIQUE
 );
 
 
 CREATE TABLE user
 (
     user_id          CHAR(36) PRIMARY KEY,
-    first_name       VARCHAR(255) NOT NULL,
-    last_name        VARCHAR(255) NOT NULL,
+    first_name       VARCHAR(255),
+    last_name        VARCHAR(255),
     date_of_birth    DATE,
     registrationDate DATE,
     FOREIGN KEY (user_id) REFERENCES user_info (user_id)
@@ -35,28 +35,28 @@ CREATE TABLE user
 CREATE TABLE department
 (
     id       CHAR(36) PRIMARY KEY,
-    projects VARCHAR(255) NOT NULL
+    projects VARCHAR(255)
 );
 
 
 CREATE TABLE company
 (
     id   CHAR(36) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    turnover DOUBLE NOT NULL,
-    expenses DOUBLE NOT NULL,
-    profit DOUBLE NOT NULL
+    name VARCHAR(255),
+    turnover DOUBLE,
+    expenses DOUBLE,
+    profit DOUBLE
 );
 
 
 CREATE TABLE employee
 (
     id            CHAR(36) PRIMARY KEY,
-    first_name    VARCHAR(255) NOT NULL,
-    last_name     VARCHAR(255) NOT NULL,
-    contact_info  VARCHAR(255) NOT NULL UNIQUE,
-    age           INT          NOT NULL,
-    hireDate      DATE         NOT NULL,
+    first_name    VARCHAR(255),
+    last_name     VARCHAR(255),
+    contact_info  VARCHAR(255),
+    age           INT,
+    hireDate      DATE,
     endDate       DATE,
     department_id CHAR(36),
     supervision   CHAR(36),
@@ -68,11 +68,11 @@ CREATE TABLE employee
 CREATE TABLE construction_project
 (
     id           CHAR(36) PRIMARY KEY,
-    project_name VARCHAR(255) NOT NULL,
-    address      VARCHAR(255) NOT NULL,
-    description  VARCHAR(255) NOT NULL,
-    startDate    DATE         NOT NULL,
-    endDate      DATE         NOT NULL,
+    project_name VARCHAR(255),
+    address      VARCHAR(255),
+    description  VARCHAR(255),
+    startDate    DATE,
+    endDate      DATE,
     manager_id   CHAR(36),
     company_id   CHAR(36),
     FOREIGN KEY (manager_id) REFERENCES employee (id),
@@ -83,14 +83,14 @@ CREATE TABLE construction_project
 CREATE TABLE tool
 (
     id            CHAR(36) PRIMARY KEY,
-    serial_number INT NOT NULL UNIQUE
+    serial_number INT
 );
 
 
 CREATE TABLE material
 (
     id                CHAR(36) PRIMARY KEY,
-    name              VARCHAR(255) NOT NULL,
-    type              VARCHAR(255) NOT NULL,
-    quantity_in_stock INT          NOT NULL
+    name              VARCHAR(255),
+    type              VARCHAR(255),
+    quantity_in_stock INT
 );
