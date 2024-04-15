74beca91f88daaf5c5d21abf9f92cd9564565163
Bolshov92 <bolshov92@gmail.com>
2024-04-15 22:56:26 +0100

    Updated schema.sql with Binary(16) ID and table existence check Changed the data type of the id column to BINARY(16) for better compatibility and efficiency. Added DROP TABLE IF EXISTS and CREATE TABLE IF NOT EXISTS statements to ensure a clean database state and improve usability.

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,38 +1,38 @@
 INSERT INTO authority (id, authority)
-VALUES ('d5b6c663-b49b-42b1-a633-ed35ea508cc0', 'User1');
+VALUES (UUID_TO_BIN('d5b6c663-b49b-42b1-a633-ed35ea508cc0'), 'User1');
 
 INSERT INTO role (role_id, role_name)
-VALUES ('d5b6c663-b49b-42b1-a633-ed35ea508cc0', 'ROLE_ADMIN'),
-       ('97caae32-94dc-433c-9f6b-4098e305eed8', 'ROLE_USER');
+VALUES (UUID_TO_BIN('d5b6c663-b49b-42b1-a633-ed35ea508cc0'), 'ROLE_ADMIN'),
+       (UUID_TO_BIN('97caae32-94dc-433c-9f6b-4098e305eed8'), 'ROLE_USER');
 
 INSERT INTO user_info (user_id, user_name, password, phone_number)
-VALUES ('a1b2c3d4-e5f6-g7h8-i9j0-123456789abc', 'admin', 'admin_password', '123456789');
+VALUES (UUID_TO_BIN('0f9368f4-518f-48cb-b917-2f0f967dd41e'), 'admin', 'admin_password', '123456789');
 
 INSERT INTO user (user_id, first_name, last_name, date_of_birth, registrationDate)
-VALUES ('a1b2c3d4-e5f6-g7h8-i9j0-123456789abc', 'John', 'Doe', '1990-01-01', '2020-01-01');
+VALUES (UUID_TO_BIN('0f9368f4-518f-48cb-b917-2f0f967dd41e'), 'John', 'Doe', '1990-01-01', '2020-01-01');
 
 INSERT INTO department (id, projects)
-VALUES ('02614a58-e256-482c-9284-c188c00a7ec6', 'Project A'),
-       ('cb5d6730-c117-473b-9ded-d4cf57a53d75', 'Project B');
+VALUES (UUID_TO_BIN('02614a58-e256-482c-9284-c188c00a7ec6'), 'Project A'),
+       (UUID_TO_BIN('cb5d6730-c117-473b-9ded-d4cf57a53d75'), 'Project B');
 
 INSERT INTO company (id, name, turnover, expenses, profit)
-VALUES ('21fbc398-0515-419e-8c57-3e3f21e554c4', 'ABC Construction', 1000000.00, 800000.00, 200000.00);
+VALUES (UUID_TO_BIN('21fbc398-0515-419e-8c57-3e3f21e554c4'), 'ABC Construction', 1000000.00, 800000.00, 200000.00);
 
 INSERT INTO employee (id, first_name, last_name, contact_info, age, hire_date, end_date, department_id)
-VALUES ('639d7bc4-9845-40aa-84c8-b0f1dced6732', 'Alice', 'Smith', 'alice@example.com', 30, '2020-01-01','2023-01-01',
-        '02614a58-e256-482c-9284-c188c00a7ec6'),
-       ('a2b4f08a-734e-42c7-83e1-ccea7d50a461', 'Jake', 'Min', 'jake@example.com', 20, '2022-01-01','2023-01-01' ,
-        '02614a58-e256-482c-9284-c188c00a7ec6');
-
-
-INSERT INTO tool (id, serial_number)
-VALUES ('8bdf32e0-0847-4bd8-8cd1-235602db6e7f', 123456),
-       ('26584493-f042-419b-8220-dea4bd8b3ff2', 789012);
-
-INSERT INTO material (id, name, type, quantity_in_stock)
-VALUES ('a7c5bfc6-8cba-4857-85de-403f613d5df7', 'Bricks', 'Building Material', 1000),
-       ('7d5a9a4b-9185-42a4-abe2-290c45de630b', 'Concrete', 'Building Material', 500);
+VALUES (UUID_TO_BIN('639d7bc4-9845-40aa-84c8-b0f1dced6732'), 'Alice', 'Smith', 'alice@example.com', 30, '2020-01-01','2023-01-01',
+        UUID_TO_BIN('02614a58-e256-482c-9284-c188c00a7ec6')),
+       (UUID_TO_BIN('a2b4f08a-734e-42c7-83e1-ccea7d50a461'), 'Jake', 'Min', 'jake@example.com', 20, '2022-01-01','2023-01-01' ,
+        UUID_TO_BIN('02614a58-e256-482c-9284-c188c00a7ec6'));
 
 INSERT INTO construction_project (id, project_name, address, description, startDate, endDate, manager_id, company_id)
-VALUES ('f2fba9a3-dbc9-4c35-8c06-9b3cc4f5b244', 'Project X', '123 Main St', 'Description of Project X', '2020-02-01',
-        NULL, '639d7bc4-9845-40aa-84c8-b0f1dced6732', '21fbc398-0515-419e-8c57-3e3f21e554c4');
+VALUES (UUID_TO_BIN('f2fba9a3-dbc9-4c35-8c06-9b3cc4f5b244'), 'Project X', '123 Main St', 'Description of Project X', '2020-02-01',
+        NULL, UUID_TO_BIN('639d7bc4-9845-40aa-84c8-b0f1dced6732'), UUID_TO_BIN('21fbc398-0515-419e-8c57-3e3f21e554c4'));
+
+INSERT INTO tool (id, serial_number)
+VALUES (UUID_TO_BIN('8bdf32e0-0847-4bd8-8cd1-235602db6e7f'), 123456),
+       (UUID_TO_BIN('26584493-f042-419b-8220-dea4bd8b3ff2'), 789012);
+
+INSERT INTO material (id, name, type, quantity_in_stock)
+VALUES (UUID_TO_BIN('a7c5bfc6-8cba-4857-85de-403f613d5df7'), 'Bricks', 'Building Material', 1000),
+       (UUID_TO_BIN('7d5a9a4b-9185-42a4-abe2-290c45de630b'), 'Concrete', 'Building Material', 500);
+
