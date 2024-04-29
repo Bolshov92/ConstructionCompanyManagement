dbff0c79f395f3211b26253723377e50f6cc3e03
Bolshov92 <bolshov92@gmail.com>
2024-04-29 22:38:43 +0100

    git add src/test/java/com/example/construction_company_management/EmployeeControllerTest.java git commit -m "Added directory for tests and EmployeeControllerTest class for testing the createEmployeePositiveTest method"

---
+++ src/test/resources/db/schemaTest.sql
@@ -0,0 +1,88 @@
+CREATE TABLE authority
+(
+    id        BINARY(16) PRIMARY KEY,
+    authority VARCHAR(255)
+);
+
+CREATE TABLE role
+(
+    role_id   BINARY(16) PRIMARY KEY,
+    role_name VARCHAR(255)
+);
+
+CREATE TABLE user_info
+(
+    user_id      BINARY(16) PRIMARY KEY,
+    user_name    VARCHAR(255),
+    password     VARCHAR(255),
+    phone_number VARCHAR(20)
+);
+
+CREATE TABLE user
+(
+    id                BINARY(16) PRIMARY KEY,
+    first_name        VARCHAR(255),
+    last_name         VARCHAR(255),
+    date_of_birth     DATE,
+    registration_date DATE,
+    user_id           BINARY(16),
+    FOREIGN KEY (user_id) REFERENCES user_info (user_id)
+);
+
+CREATE TABLE department
+(
+    id       BINARY(16) PRIMARY KEY,
+    dep_name VARCHAR(255)
+);
+
+CREATE TABLE company
+(
+    id   BINARY(16) PRIMARY KEY,
+    name VARCHAR(255),
+    turnover DOUBLE,
+    expenses DOUBLE,
+    profit DOUBLE
+);
+
+CREATE TABLE employee
+(
+    id            BINARY(16) PRIMARY KEY,
+    first_name    VARCHAR(255),
+    last_name     VARCHAR(255),
+    contact_info  VARCHAR(255),
+    age           INT,
+    hire_date     DATE,
+    end_date      DATE,
+    department_id BINARY(16),
+    supervision   BINARY(16),
+    FOREIGN KEY (department_id) REFERENCES department (id),
+    FOREIGN KEY (supervision) REFERENCES employee (id)
+);
+
+CREATE TABLE construction_project
+(
+    id           BINARY(16) PRIMARY KEY,
+    project_name VARCHAR(255),
+    address      VARCHAR(255),
+    description  VARCHAR(255),
+    startDate    DATE,
+    endDate      DATE,
+    manager_id   BINARY(16),
+    company_id   BINARY(16),
+    FOREIGN KEY (manager_id) REFERENCES employee (id),
+    FOREIGN KEY (company_id) REFERENCES company (id)
+);
+
+CREATE TABLE tool
+(
+    id            BINARY(16) PRIMARY KEY,
+    serial_number INT
+);
+
+CREATE TABLE material
+(
+    id                BINARY(16) PRIMARY KEY,
+    name              VARCHAR(255),
+    type              VARCHAR(255),
+    quantity_in_stock INT
+);
