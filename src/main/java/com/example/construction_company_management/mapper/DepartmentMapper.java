ecaec0b4e35fc32e3799b7f4c4c4e4d850bbba6f
Bolshov92 <bolshov92@gmail.com>
2024-05-28 22:27:01 +0100

    Added NOT NULL and UNIQUE constraints to tables

--- src/main/java/com/example/construction_company_management/mapper/DepartmentMapper.java
+++ src/main/java/com/example/construction_company_management/mapper/DepartmentMapper.java
@@ -7,16 +7,18 @@ import com.example.construction_company_management.dto.DepartmentUpdateDto;
 import com.example.construction_company_management.entity.Department;
 import org.mapstruct.*;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DepartmentMapper {
 
     @Mapping(target = "depName", source = "depName")
+    @Mapping(target = "employees", ignore = true)
     Department toEntity(DepartmentCreateDto departmentCreateDto);
 
     @Mapping(target = "departmentId", source = "depName")
     DepartmentAfterCreationDto toDo(Department departmentAfterCreation);
 
     @Mapping(target = "depName", source = "departmentUpdateDto.depName")
+    @Mapping(target = "employees", ignore = true)
     void toUpdate(DepartmentUpdateDto departmentUpdateDto, @MappingTarget Department department);
 
     @Mapping(target = "departmentId", source = "id")
